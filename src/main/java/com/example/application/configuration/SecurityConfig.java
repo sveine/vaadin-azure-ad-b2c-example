754a1c8cb9d25d1c79e6226bf93d05897d8984f2
svein <svein@jacilla.no>
2022-10-04 14:12:57 +0200

    Try to use setOAuth2LoginPage(). Now /logout does not work

--- src/main/java/com/example/application/configuration/SecurityConfig.java
+++ src/main/java/com/example/application/configuration/SecurityConfig.java
@@ -5,6 +5,7 @@ package com.example.application.configuration;
 
 import com.azure.spring.cloud.autoconfigure.aadb2c.AadB2cOidcLoginConfigurer;
 import com.vaadin.flow.spring.security.VaadinWebSecurity;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
@@ -12,14 +13,17 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 public class SecurityConfig extends VaadinWebSecurity {
 
     private final AadB2cOidcLoginConfigurer configurer;
+    private final String signUpOrSignInFlowName;
 
-    public SecurityConfig(AadB2cOidcLoginConfigurer configurer) {
+    public SecurityConfig(AadB2cOidcLoginConfigurer configurer, Environment environment) {
         this.configurer = configurer;
+        signUpOrSignInFlowName = "/oauth2/authorization/" + environment.getProperty("spring.cloud.azure.active-directory.b2c.user-flows.sign-up-or-sign-in");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
         http.apply(configurer);
+        setOAuth2LoginPage(http, signUpOrSignInFlowName);
     }
 }
